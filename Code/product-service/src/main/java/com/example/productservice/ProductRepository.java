9cc79848e4431a9a8cf4de71a3c7022cb9839dc8
aymen drira <sjdfmoizjef@gmail.com>
2024-01-13 18:48:19 +0100

    microservice notification

--- Code/product-service/src/main/java/com/example/productservice/ProductRepository.java
+++ Code/product-service/src/main/java/com/example/productservice/ProductRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product,Integer> {
+
+
+
+
 }
