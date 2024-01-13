01aa04255f063a8998a4852d0efa82c36d1a8d22
aymen drira <sjdfmoizjef@gmail.com>
2024-01-13 15:54:46 +0100

    Rajout de la config JPA et connexion a la base

---
+++ Code/product-service/src/main/java/com/example/productservice/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.productservice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product,Integer> {
+}
